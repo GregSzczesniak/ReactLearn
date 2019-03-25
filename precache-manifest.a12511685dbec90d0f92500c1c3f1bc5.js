ce319ffbe29ce66f5dcd3c1bb286e56705a45cea
greg <greg@macbook-air-greg.home>
2019-03-25 20:21:20 +0100

    Updates

---
+++ precache-manifest.a12511685dbec90d0f92500c1c3f1bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1b2ccccf5b295a46063",
+    "url": "/ReactLearn/static/js/runtime~main.f0db4aec.js"
+  },
+  {
+    "revision": "f65913ad9ef14c3c0150",
+    "url": "/ReactLearn/static/js/main.ebd90066.chunk.js"
+  },
+  {
+    "revision": "b548e30793887743f0b8",
+    "url": "/ReactLearn/static/js/2.cf331bc2.chunk.js"
+  },
+  {
+    "revision": "f65913ad9ef14c3c0150",
+    "url": "/ReactLearn/static/css/main.96f4f2d5.chunk.css"
+  },
+  {
+    "revision": "856845b0f51d7d818319a91eba992445",
+    "url": "/ReactLearn/index.html"
+  }
+];
